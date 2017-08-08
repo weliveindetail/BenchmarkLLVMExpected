5440ffd2fa08037fa6969aac5eede6372d388839
stefan <stefan.graenitz@gmail.com>
2017-08-08 11:33:18 +0200

    Remove bool data benchmarks as it basically duplicates int data results

--- BenchmarkExpected.cpp
+++ BenchmarkExpected.cpp
@@ -1,26 +1,10 @@
-#include "bool_bench.h"
 #include "int_bench.h"
 #include "unique_ptr_bench.h"
 #include "vector_bench.h"
 
 #include <benchmark/benchmark.h>
 
-// bool data
-BENCHMARK(BM_Bool_SuccessAlways_IgnoreErr)->UseManualTime();
-
-BENCHMARK(BM_Bool_SuccessAlways_ErrorCode)->UseManualTime();
-BENCHMARK(BM_Bool_Success2outOf3_ErrorCode)->UseManualTime();
-BENCHMARK(BM_Bool_Success1outOf3_ErrorCode)->UseManualTime();
-BENCHMARK(BM_Bool_SuccessNever_ErrorCode)->UseManualTime();
-
-BENCHMARK(BM_Bool_SuccessAlways_Expected)->UseManualTime();
-BENCHMARK(BM_Bool_Success2outOf3_Expected)->UseManualTime();
-BENCHMARK(BM_Bool_Success1outOf3_Expected)->UseManualTime();
-BENCHMARK(BM_Bool_SuccessNever_Expected)->UseManualTime();
-
 // int data
-BENCHMARK(BM_Int_SuccessAlways_IgnoreErr)->UseManualTime();
-
 BENCHMARK(BM_Int_SuccessAlways_ErrorCode)->UseManualTime();
 BENCHMARK(BM_Int_Success2outOf3_ErrorCode)->UseManualTime();
 BENCHMARK(BM_Int_Success1outOf3_ErrorCode)->UseManualTime();
